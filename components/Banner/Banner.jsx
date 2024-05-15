2bb979bf4fac7f7851a3862696d91653b7255eab
Tatyana Tupalo <tatyana080489t@gmail.com>
2024-05-15 12:42:32 +0300

    fix background

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -6,6 +6,8 @@ import s from "./Banner.module.scss";
 const Banner = ({ children }) => {
   return (
     <div className={s.banner}>
+      <div className={s.spot_top}></div>
+      <div className={s.spot_bottom}></div>
       <Parallax pages={2.6}>
         <ParallaxLayer offset={0} speed={-0.8} className={s.moon} />
         <ParallaxLayer speed={0.8} factor={1}>
